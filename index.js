c667d98d08a18d14a2cca625f09a6355ff11c4e5
Elizabeth <ek@elizabethkillgore.com>
2023-07-02 10:10:09 -0500

    difficult lab in functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork()
+mondayWork("fly to Atlanta")
+
+
+
+function wrapAdjective(flair = "*") {
+    return function (compliment = "special") {
+
+        return `You are ${flair}${compliment}${flair}!`;
+    }
+}
+wrapAdjective("$")("talented");
+
